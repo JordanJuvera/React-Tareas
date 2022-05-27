0cc88ba11fb2d38b849dafae1d6bbf6e376ff0ff
JordanJuvera <juvera.jordan@gmail.com>
2022-05-27 17:54:23 -0500

    Proyecto final: Listar-Agregar Tareas

---
+++ src/componentes/TareaFormulario.jsx
@@ -0,0 +1,42 @@
+import  React,  {useState}from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import '../hojas-de-estilo/TareaFormulario.css'
+
+function TareaFormulario(props){
+
+    const [input,setInput] = useState('');
+
+    const manejarCambio = e =>{
+        setInput(e.target.value);
+        
+    };
+
+    const manejarEnvio = e => {
+        e.preventDefault();
+        console.log("Enviando formulario")
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: input,
+            completada: false
+        }
+
+        props.onSubmit(tareaNueva);
+    };
+    
+    return (
+        <form className='tarea-formulario' onSubmit={manejarEnvio}>
+            <input 
+                className='tarea-input'
+                type='text'
+                placeholder='Escribe una Tarea'
+                name='texto'
+                onChange={manejarCambio}
+            />
+            <button className='tarea-boton'>
+                Agregar tarea
+            </button>
+        </form>
+    );
+}
+
+export default TareaFormulario;
